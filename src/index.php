9a0c0b6d402082ac04650bdd31d69d9c2945fc94
Noel <noel.g.castro@gmail.com>
2026-07-13 16:27:06 +0800

    first commit

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@ echo "test01";
 $host = "66.113.234.121";
 $user = "comelec";
 $password = "Default123!@#";
-$database = docker;
+$database = "docker";
 $port = 3306;
 
 $con = new mysqli($host, $user, $password, $database, $port);
