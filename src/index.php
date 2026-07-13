6b84203521a29fb1d5de516789e793e9e6a90aaa
Noel <noel.g.castro@gmail.com>
2026-07-13 16:44:48 +0800

    first commit

--- src/index.php
+++ src/index.php
@@ -1,12 +1,12 @@
 <?php
 echo "test01";
 $host = "66.113.234.121";
-$user = "root";
+$user = "comelec";
 $password = "Default123!@#";
 $database = "comelec";
 $port = 3306;
 
-$con = new mysqli($host, $user, $password, $database, $port);
+$conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error){
     die ("Connection failed:" . $conn->connect_error);
